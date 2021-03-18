e25a068169a1cb7e7c6abbd1db4c523886f1b0f5
Gorbunov-PI18-3 <stfouley5@gmail.com>
2021-03-18 15:00:07 +0300

    Добавьте файлы проекта.

---
+++ Sem14/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sem14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var phoneBook = new List<Contact>();
+
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var sorted = from s in phoneBook orderby s.Name, s.LastName select s;
+
+            foreach (var phone in sorted)
+                Console.WriteLine(phone.Name + " " + phone.LastName);
+        }
+    }
+
+
+
+    public class Contact
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email)
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+
+    }
+
+
+
+}
